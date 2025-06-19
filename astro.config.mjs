47e0b1ee4031b8c83f60b6e6e97ed5150e0d27ae
Undead34 <maizogabriel@gmail.com>
2025-06-19 09:37:49 -0400

    refactor: add good path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    publicDir: './desktop-app/frontend/public',
+    srcDir: './desktop-app/frontend/src',
+    vite: {
+        plugins: [tsconfigPaths()],
+    },
+});
